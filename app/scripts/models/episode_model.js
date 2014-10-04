31d9160a0ddd7bceca44a334f099c441a4b630f6
Matthew Noorenberghe <github@matthew.noorenberghe.com>
2014-10-04 16:03:25 -0700

    WIP: use device storage with a tiemstamped file name

--- app/scripts/models/episode_model.js
+++ app/scripts/models/episode_model.js
@@ -6,16 +6,14 @@ HighFidelity.Episode = DS.Model.extend({
     audioLength: DS.attr('number'),
     playbackPosition: DS.attr('number'),
     playCount: DS.attr('number'),
-    // audioFile: DS.attr('object'),
+    audioFileName: DS.attr('string'),
     guid: DS.attr('string'),
 
     // Episode metadata from RSS.
     datePublished: DS.attr('number'),
 
     // Episode download data; unavailable in hosted version for the time being.
-    isDownloaded: DS.attr('boolean'),
-    _chunkCount: DS.attr('number'),
-    _chunkCountSaved: DS.attr('number'),
+    isDownloaded: DS.attr('boolean'), // TODO: check the file exists on disk
     _loadComplete: false,
 
     // canDownload: function() {
@@ -33,17 +31,22 @@ HighFidelity.Episode = DS.Model.extend({
         var _this = this;
 
         return new Promise(function(resolve) {
-            _this._assembleChunkData().then(function(blob) {
-                resolve(window.URL.createObjectURL(blob));
-            });
+            var music = navigator.getDeviceStorage('music');
+            var fileRequest = music.get(_this.get("audioFileName"));
+
+            fileRequest.onsuccess = function () {
+                resolve(window.URL.createObjectURL(this.result));
+            };
+
+            fileRequest.onerror = function () {
+                console.warn("Unable to get the file: ", this.error);
+            };
         });
     },
 
     // Download the episode for local playback.
     download: function(model) {
         this.set('isDownloading', true);
-        this.set('_chunkCount', 0);
-        this.set('_chunkCountSaved', 0);
 
         console.log(this, model);
 
@@ -51,42 +54,34 @@ HighFidelity.Episode = DS.Model.extend({
         var request = new XMLHttpRequest({mozSystem: true});
 
         request.open('GET', this.get('audioURL'), true);
-        request.responseType = 'moz-chunked-arraybuffer';
+        request.responseType = 'blob';
 
-        // request.addEventListener('load', );
         request.addEventListener('load', function() {
-            console.log(request.readyState, request);
-            if (request.readyState === 4) {
-                // _this._setAudioType(_this.get('audioURL'));
-                _this._loadComplete = true;
-            }
-        });
+            // _this._setAudioType(_this.get('audioURL'));
 
-        request.addEventListener('progress', function(event) {
-            console.info('eventProgress', _this.get('_chunkCount'));
-            localforage.setItem('ep:' + _this.get('id') +
-                                _this.get('_chunkCount'), request.response)
-                       .then(function() {
-                // Increment our internal data chunk count.
-                _this.incrementProperty('_chunkCountSaved');
+            var music = navigator.getDeviceStorage("music");
+            // TODO: real name and/or folder structure.
+            // * Use responseURL if it exists otherwise fallback to URL.
+            // TODO: Use the correct extension. Don't assume MP3.
+            // TODO: Uniqueness to avoid overwrite.
+            var filename = "podcast-" + Date.now() + ".mp3";
+            var saveRequest = music.addNamed(request.response, filename);
 
-                if (_this.get('_chunkCount') ===
-                    _this.get('_chunkCountSaved') &&
-                    _this.get('_loadComplete')) {
-                    console.log('chunk data assembled');
-                    _this.set('isDownloading', false);
-                    _this.set('isDownloaded', true);
-                    // try {
-                    //     setTimeout(function() {
-                    //         _this.save();
-                    //     }, 20);
-                    // } catch (e) {
-                    //     console.error(e);
-                    // }
-                }
-            });
+            saveRequest.onsuccess = function() {
+                var name = this.result;
+                console.log('File "' + name + '" successfully wrote to the music storage area');
+                _this.set('audioFileName', filename);
+                _this.save();
+            };
 
-            _this.incrementProperty('_chunkCount');
+            // An error typically occur if a file with the same name already exist
+            saveRequest.onerror = function() {
+                console.warn('Unable to write the file: ' + this.error);
+            };
+
+            _this._loadComplete = true;
+            _this.set('isDownloading', false);
+            _this.set('isDownloaded', true);
         });
 
         request.addEventListener('error', function(event) {
@@ -96,50 +91,6 @@ HighFidelity.Episode = DS.Model.extend({
         });
 
         request.send(null);
-    },
-
-    // Because of limitations/platform bugs in b2g18 (what ships on
-    // first-gen Firefox OS devices), we can't move around huge Blob
-    // objects (particularly, we can't save them anywhere) without crashing
-    // the app on a device from out of memory errors.
-    //
-    // This retrieves the smaller, more manageable chunks of data from
-    // IndexedDB and assembles them into a Blob object we hand off to a
-    // callback function. For some reason, B2G seems to cope just fine with
-    // this very odd solution because it handles Blobs full of arrayBuffers
-    // better than it saves large Blobs.
-    //
-    // This is a HACK and should be fixed by platform in the next release,
-    // I hope.
-    //
-    // See: https://bugzilla.mozilla.org/show_bug.cgi?id=873274
-    // and: https://bugzilla.mozilla.org/show_bug.cgi?id=869812
-    _assembleChunkData: function() {
-        var audioBlobs = [];
-        var chunkCount = this.get('_chunkCount');
-        var _this = this;
-
-        return new Promise(function(resolve) {
-            function _walkChunks(chunkID) {
-                if (chunkID === undefined) {
-                    chunkID = 0;
-                }
-
-                if (chunkID < chunkCount) {
-                    localforage.getItem('ep:' + _this.get('id') + chunkID)
-                               .then(function(blob) {
-                        console.info('ep:' + _this.get('id') + chunkID, blob);
-                        audioBlobs.push(blob);
-                        _walkChunks(chunkID + 1);
-                    });
-                } else {
-                    var blob = new Blob(audioBlobs, {type: 'mp3'});
-                    resolve(blob);
-                }
-            }
-
-            _walkChunks();
-        });
     },
 
     // Set the audio type based on the responseType (or filename) of this
